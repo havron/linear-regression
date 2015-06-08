4e7735964822ce4c647e9d99f63aa71de402774b
Samuel Havron <havron@virginia.edu>
2015-06-08 13:34:23 -0400

    Attempt at fixed point arithmetic.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Linear-regression
-Development of Obliv-C and C linear regression program for developing secure multiparty computations.
+Development of Obliv-C and C linear regression program for developing secure multiparty computations. Fixed point arithmetic is used in order to support Obliv-C.
 
 To use, compile with `make linReg` and run with `./linReg <filename>`. 
-Run the [test code](test.data) with `./linReg test.data`.
+Run the [test data](test.data) with `./linReg test.data`.
 
 To contact me with any questions about this repository, open an issue on GitHub or email me at havron@virginia.edu.
+
+# Warnings
+First revision of [code](linReg.c) with fixed point arithmetic. DOES NOT COMPILE. Use previous commit to compile.
--- linReg.c
+++ linReg.c
@@ -7,30 +7,34 @@
 #include <math.h>
 #define MAX_DATA 100
 
+// Fixed point arithmetic
+#define SHIFT_AMOUNT 16 // 2^16 = 65536
+#define SHIFT_MASK ((1 << SHIFT_AMOUNT) - 1) // 65535, draw out fraction with bitwise '&'
+
 typedef struct {
-  double x[MAX_DATA]; // independent points
-  double y[MAX_DATA]; // dependent points
+  int x[MAX_DATA]; // independent points
+  int y[MAX_DATA]; // dependent points
   int n; // number of data points
 } DataSet;
 
 typedef struct {
-  double m; // slope
-  double b; // y-intercept
-  double r; // correlation constant
+  int m; // slope
+  int b; // y-intercept
+  int r; // correlation constant
 } LinRegResult;
 
 // forward declarations
 DataSet load_data(FILE *inputFile);
 LinRegResult linear_regression(DataSet theData);
-double sqr(double x);
-double dotProd(double a[MAX_DATA], double b[MAX_DATA], int n);
-double sum(double a[MAX_DATA], int n);
+int sqr(int x);
+int dotProd(int a[MAX_DATA], int b[MAX_DATA], int n);
+int sum(int a[MAX_DATA], int n);
 
 DataSet load_data(FILE *inputFile) {
   int i;
   DataSet data;
   for (i = 0; i < MAX_DATA; ++i) {
-    int dataPoints = fscanf(inputFile, "%lf %lf", &data.x[i], &data.y[i]);
+    int dataPoints = fscanf(inputFile, "%lf %lf", &data.x[i], &data.y[i]); // How to convert to fixed point arithmetic?
     if (dataPoints != 2) {
       if (dataPoints < 0 && feof(inputFile)) {
 	break;
@@ -49,16 +53,16 @@ LinRegResult linear_regression(DataSet theData) {
 
   LinRegResult result;
   int n = theData.n; // number of data points
-  double sumx = sum(theData.x, n); // sum of x
-  double sumxx = dotProd(theData.x, theData.x, n); // sum of each x sqaured
-  double sumy = sum(theData.y, n); //sum of y
-  double sumyy = dotProd(theData.y, theData.y, n); // sum of each y squared
-  double sumxy = dotProd(theData.x, theData.y, n); // sum of each x * y
+  int sumx = sum(theData.x, n); // sum of x
+  int sumxx = dotProd(theData.x, theData.x, n); // sum of each x squared
+  int sumy = sum(theData.y, n); //sum of y
+  int sumyy = dotProd(theData.y, theData.y, n); // sum of each y squared
+  int sumxy = dotProd(theData.x, theData.y, n); // sum of each x * y
 
   // Compute least-squares best fit straight line
-  result.m = (n * sumxy - sumx * sumy) / (n * sumxx - sqr(sumx)); // slope
-  result.b = (sumy * sumxx - sumx * sumxy) / (n * sumxx - sqr(sumx)); // y-intercept
-  result.r = (sumxy - sumx * sumy / n) / sqrt((sumxx - sqr(sumx) / n) * (sumyy - sqr(sumy) / n)); // correlation
+  result.m = ((n * sumxy - sumx * sumy) << SHIFT_AMOUNT) / ((n * sumxx - sqr(sumx)) << SHIFT_AMOUNT); // slope
+  result.b = ((sumy * sumxx - sumx * sumxy) << SHIFT_AMOUNT) / ((n * sumxx - sqr(sumx)) << SHIFT_AMOUNT); // y-intercept
+  result.r = ((sumxy - sumx * sumy) << SHIFT_AMOUNT / n) / sqrt(((sumxx - sqr(sumx)) << SHIFT_AMOUNT / n) * ((sumyy - sqr(sumy)) << SHIFT_AMOUNT / n)); // correlation
 
   return result;
 }
@@ -80,9 +84,9 @@ int main(int argc, char *argv[]) {
       fclose(input);
      
       LinRegResult linReg = linear_regression(theData);
-      printf("\nSlope   \tm = %15.6e\n", linReg.m); // print slope
-      printf("y-intercept\tb = %15.6e\n", linReg.b); // print y-intercept
-      printf("Correlation\tr = %15.6e\n", linReg.r); // print correlation
+      printf("\nSlope   \tm = %d\n", linReg.m >> SHIFT_AMOUNT); // print slope
+      printf("y-intercept\tb = %d\n", linReg.b >> SHIFT_AMOUNT); // print y-intercept
+      printf("Correlation\tr = %d\n", linReg.r >> SHIFT_AMOUNT); // print correlation
     }
 
   } else {
@@ -92,24 +96,24 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-double sqr(double x) {
+int sqr(int x) {
   return x * x;
 }
 
-double dotProd(double a[MAX_DATA], double b[MAX_DATA], int n) {
-  double dotProd = 0;
+int dotProd(int a[MAX_DATA], int b[MAX_DATA], int n) {
+  int dotProd = 0;
   int i;
   for (i = 0; i < n; i++) {
-    dotProd += a[i] * b[i];
+    dotProd += (a[i] * b[i]) << SHIFT_AMOUNT;
   }
   return dotProd;
 }
 
-double sum(double a[MAX_DATA], int n) {
-  double sum = 0;
+int sum(int a[MAX_DATA], int n) {
+  int sum = 0;
   int i;
   for (i = 0; i < n; i++) {
-    sum += a[i];
+    sum += a[i] << SHIFT_AMOUNT;
   }
   return sum;
 }
